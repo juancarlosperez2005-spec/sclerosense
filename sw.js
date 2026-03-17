405fd645dd4ca0e2efaa4d8f147d3b36c8406290
juancarlosperez2005-spec <juancarlosperez2005@gmail.com>
2026-03-17 09:35:56 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE = 'sclerosense-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'sclerosense-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
